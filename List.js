7f4d349d02018fe66ee0b65277ae98680aa9fd2c
White <White@WhitedeMacBook-Pro.local>
2019-05-17 12:53:32 +1000

    format adjusted

--- List.js
+++ List.js
@@ -1,35 +1,38 @@
-const url ='https://us-west-2.cloudconformity.com/v1/services';
-async function createList(){
+const url = 'https://us-west-2.cloudconformity.com/v1/services';
+async function createList() {
+      // fetch data     
       const res = await fetch(url);
-      const  apiData = await res.json();    
-      console.log(apiData);     
+      const apiData = await res.json();
+      console.log(apiData);
+      //   sort data by  alphabetical order in the  first array
       function sortByKey(array, key) {
-            return array.sort(function(a, b) {
-            var x = a[key]; var y = b[key];
-            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            return array.sort(function (a, b) {
+                  var x = a[key]; var y = b[key];
+                  return ((x < y) ? -1 : ((x > y) ? 1 : 0));
             });
-            }   
-      sortByKey(apiData.data, 'id');     
-      console.log(apiData.data);  
-      let output = '<div class="list-container">' 
+      }
+      sortByKey(apiData.data, 'id');
+      console.log(apiData.data);
+      let output = '<div class="list-container">'
+      // show data as a list
       apiData.data.forEach(
-            service=>{
-                  output +=  `<div class='wrap'><h3><a href="https://www.cloudconformity.com/conformity-rules/${service.id}">${service.id}</a></h3>`
+            service => {
+                  output += `<div class='wrap'><h3><a href="https://www.cloudconformity.com/conformity-rules/${service.id}">${service.id}</a></h3>`
                   service.relationships.rules.data.forEach(
-                        rule=>{                    
+                        rule => {
                               apiData.included.forEach(
-                                   content=>{
-                                         if(content.id===rule.id){
-                                          output +=  ` <div><a href="https://www.cloudconformity.com/conformity-rules/${service.id}/${content.attributes.slug}.html">
+                                    content => {
+                                          if (content.id === rule.id) {
+                                                output += ` <div><a href="https://www.cloudconformity.com/conformity-rules/${service.id}/${content.attributes.slug}.html">
                                           ${content.attributes.title}</a></div>`
-                                   }
-                                   })
+                                          }
+                                    })
                         }
                   )
-                  output +=  `</div>`
-            } 
-         )
-         output +=  `</div>`;
-         document.getElementById('grid').innerHTML= output;         
- }
- createList();
+                  output += `</div>`
+            }
+      )
+      output += `</div>`;
+      document.getElementById('grid').innerHTML = output;
+}
+createList();
